0288d30b87bdc02fe88f957bfaf92adce814bbbb
Ayush Pai <ayushpai@gmail.com>
2025-06-21 14:16:14 -0700

    Initial Commit

---
+++ pathvoice/pathvoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  pathvoiceApp.swift
+//  pathvoice
+//
+//  Created by Ayush Pai on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct pathvoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pathvoiceTests/pathvoiceTests.swift
@@ -0,0 +1,16 @@
+//
+//  pathvoiceTests.swift
+//  pathvoiceTests
+//
+//  Created by Ayush Pai on 6/21/25.
+//
+
+import Testing
+
+struct pathvoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
